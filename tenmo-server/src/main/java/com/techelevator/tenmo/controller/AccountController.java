330766e383253f896d8d0143a2c74cbfc9c488fc
Robert Gates <robertgates312@gmail.com>
2022-03-10 10:00:13 -0500

    Controller Stuff and Tests

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -4,6 +4,12 @@ import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountController {
     private final AccountDao accountDao;
@@ -17,6 +23,10 @@ public class AccountController {
     }
 
     //listUsers() Robert
+    @RequestMapping(path = "/users", method = RequestMethod.GET)
+    public List<User> listUsers(){
+        return userDao.findAll();
+    }
 
 
 
--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -3,6 +3,14 @@ package com.techelevator.tenmo.controller;
 import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.exception.TransferNotFoundException;
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.util.List;
 
 public class TransferController {
     private final AccountDao accountDao;
@@ -16,12 +24,22 @@ public class TransferController {
     }
 
     // listTransfers(Principal principal) Robert
+    @RequestMapping(path = "/transfers", method = RequestMethod.GET)
+    public List<Transfer> transferList(Principal principal){
+        return transferDao.getAllTransfersForUser(userDao.findIdByUsername(principal.getName()));
+    }
 
 
     // completeTransfer(int fromAccountID, int toAccountId, double amount) Scott
 
 
     // getTransferById(int transferId) Robert
+    @RequestMapping(path = "/transfers/{id}", method = RequestMethod.GET)
+    public Transfer getTransferById(@PathVariable("id") int transferID) throws TransferNotFoundException {
+        return transferDao.getTransferById(transferID);
+
+
+    }
 
 
     // private createTransfer(Transfer transfer) Scott
